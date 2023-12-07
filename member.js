e7d012546e7d0abc76e40817940300b3524eba17
Rui <rui.hunag17@hotmail.com>
2023-12-06 23:09:04 -0500

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberBox = document.getElementById("memberBox");
+    var skillsBox = document.getElementById("skillsBox");
+    var projectsBox = document.getElementById("projectsBox");
+    var contactBox = document.getElementById("contactBox");
+    var aboutBox = document.getElementById("aboutBox");
+    member.style.color = "rgb(255, 255, 255)";
+    skills.style.color = "rgb(255, 255, 255)";
+    projects.style.color = "rgb(255, 255, 255)";
+    contact.style.color = "rgb(255, 255, 255)";
+    about.style.color = "rgb(255, 255, 255)";
+    memberBox.style.display = "block";
+    skillsBox.style.display = "none";
+    projectsBox.style.display = "none";
+    contactBox.style.display = "none";
+    aboutBox.style.display = "none";
+}
